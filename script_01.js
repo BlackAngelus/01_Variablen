5d8c8b3abf61652be186bbd55c8f1bb6a547fed9
Florian <94174007+BlackAngelus@users.noreply.github.com>
2021-11-19 09:19:11 +0100

    Update script_01.js

--- script_01.js
+++ script_01.js
@@ -1,73 +1,56 @@
 /***** Variablen 01 *******/
 
 /**
+ * 
  *  Block-Kommentar
+ * 
  */
 
-//  Zeilen-Kommentar
+// Zeilen-Kommentar
 
-//  Konsolen-Ausgabe
+// Konsolen-Ausgabe
 // console.log("Hello, World!");
-// console.log(firstName);                                          // nicht deklarierte Variante --> Fehler
+// console.log(firstName); // nicht deklarierte Var. --> Fehler
 
-/***** 02 Deklaration + Wertzuweisung I *****/
+/***** 02 Deklaration + Wertzuweisung I *******/
 
-// let firstName;                                                   // Deklaration (Definition) in JS5 var
-// firstName = "Horst";                                             // Wertzuweisung
-// console.log(firstName);                                          // Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
 
-// let familyName = "Schlemmer"                                     //Deklaration + Wertzuweisung
-// console.log(familyName);                                         // Ausgabe
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log(familyName); // Ausgabe
 // console.log(firstName + " " + familyName);
 
-/***** 03 Deklaratoion + Wertzuweisung II *****/
+/***** 03 Deklaration + Wertzuweisung II *******/
 
 // let firstName, familyName;
-// firstName = prompt("Bitte vornamen eingeben");
-// familyName = prompt("Bitte Nachnamen eingeben");
-// console.log(firstName + " " + familyName);
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+// console.log(firstName + " " + familyName);s
 
-// JS ist ein untypisierte Sprache! | untyped
+// JS ist eine untypisierte Sprache! | untyped
 
 // let test;
-// test ="Hi"
+// test = "hi";
 // test = 2;
 // test = true;
 // console.log("Datentyp: " + typeof test);
 // console.log("Inhalt: " + test);
 
+/***** 03a Konstanten *******/
+// const test = "hi"; // Variable mit KONSTANTEM Inhalt
+// test = "hallo"; // KEINE neue Zuweisung zur LZ möglich!
+// console.log("Inhalt: " + test);
 
-/***** 03 Konstanten *****/
-// const test;                                 //Variable mit KONSTANTEM Inhalt
-// test = "hi";                                // KEINE neue Zuweisung zur LZ möglich!
-// console.log("inhalt: " + test);
-
-/***** Beispiel I *****/
-
-// Deklaration
-// let ageJohn, ageMark;
-// let birthYearJohn, birthYearMark;
-// let year = 2021;
-
-// // Wertzuweisung
-// ageJohn = 25;
-// ageMark = 30;
-
-// // Berechnung
-// birthYearJohn = year - ageJohn;
-// birthYearMark = year - ageMark;
-
-// // Ausgabe
-// console.log("birthYearJohn: " + birthYearJohn);
-// console.log("birthYearMark: " + birthYearMark);
-
-/***** Beispiel II *****/
+/***** Beispiel *******/
 
 // Deklaration
 let ageJohn, ageMark;
 let birthYearJohn, birthYearMark;
 
-let date = new Date ();
+// let year = 2021;
+let date = new Date();
 let year = date.getFullYear();
 
 // Wertzuweisung
@@ -75,11 +58,9 @@ ageJohn = 25;
 ageMark = 30;
 
 // Berechnung
-birthYearJohn = date.getFullYear() - ageJohn;
-birthYearMark = date.getFullYear() - ageMark;
+birthYearJohn = year - ageJohn;
+birthYearMark = year - ageMark;
 
 // Ausgabe
 console.log("birthYearJohn: " + birthYearJohn);
 console.log("birthYearMark: " + birthYearMark);
-
-test
